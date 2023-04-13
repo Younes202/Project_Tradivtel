89634483b3fc96728253085aa120222b536cf651
Younes202 <sghyeryounes@gmail.com>
2023-04-13 19:51:55 +0000

    COmmit about HTML AUthentication

--- login/views.py
+++ login/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 # Create your views here.
-def index(request):
-    return HttpResponse('Hello world')
+def login(request):
+    return render(request, 'HTML/LOGIN/authentication.html')
+
+
+def signup(request):
+    firstname = request.POST.get('firstName')
+    lastname = request.POST.get('lastName')
+    email = request.POST.get('email')
+    password = request.POST.get('confirmPassword')
+    user = {'firstName': firstname, 'lastName': lastname, 'email': email, 'confirmPassword': password}
+    return render(request, '/Test.html', user)
